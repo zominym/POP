eb2bc528cc5cd1d1afb2865343dc048fa4ae5425
zominym <marc.zominy@etu.univ-lyon1.fr>
2016-03-07 08:58:06 +0100

    ajout POPUI

---
+++ src/POPUI.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class POPUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner keyboard = new Scanner(System.in);
+		int srvPort = 110;
+		
+		System.out.println("WELCOME TO ZGUYL POP3 MAIL SERVICE");
+		System.out.println("PLEASE TYPE SERVER ADDRESS");
+		String srvAddress = keyboard.nextLine();
+		
+		System.out.println("PLEASE TYPE USER NAME");
+		String usrName = keyboard.nextLine();
+		
+		System.out.println("PLEASE TYPE USER PASSWORD");
+		String usrPass = keyboard.nextLine();
+		
+		
+		
+		
+		System.out.println("TRYING TO CONNECT USING :");
+		System.out.println(usrName + '@' + srvAddress + ':' + srvPort + " ***" + usrPass + "***");
+		
+	}
+
+}
