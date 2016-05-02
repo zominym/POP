1c7f38800acdcaba08eca78abedb6b22c39b4fe5
zominym <marc.zominy@etu.univ-lyon1.fr>
2016-05-02 09:48:29 +0200

    improved GUI

--- src/POP.java
+++ src/POP.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.IOException;
 
 
@@ -7,9 +8,11 @@ public class POP {
 		// TODO Auto-generated method stub
 
 		POPGUI dialog = new POPGUI();
-		dialog.output.setText("<html>Bienvenue sur le serveur mail ZGUYL <br><br> Veuillez entrer vos identifiants de connexion");
+		dialog.output.setText("Bienvenue sur le serveur mail ZGUYL \n\n Veuillez entrer vos identifiants de connexion");
 		dialog.pack();
-		dialog.setSize(800,600);
+		dialog.setSize(1200,600);
+		dialog.setMinimumSize(new Dimension(600, 500));
+		dialog.setResizable(true);
 		dialog.setVisible(true);
 		System.exit(0);
 	}
